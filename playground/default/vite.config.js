a86af38afa68022da69c48ae003bf7803a5e81db
Ignácz Dominik Bence <17203874+ignaczdominik@users.noreply.github.com>
2023-05-14 12:50:14 +0000

    version 23.05.19 snapshot

--- playground/default/vite.config.js
+++ playground/default/vite.config.js
@@ -2,14 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), eslint()],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '~bootstrap': fileURLToPath(new URL('./node_modules/bootstrap', import.meta.url))
     }
   },
   server: {
